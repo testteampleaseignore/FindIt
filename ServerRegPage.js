e011b948f95f9c70c07c970057e924524a9f57d1
mate6483 <47071634+mate6483@users.noreply.github.com>
2019-03-12 19:26:36 -0600

    Add files via upload

---
+++ ServerRegPage.js
@@ -0,0 +1,31 @@
+/***********************
+  Load Components!
+
+  Express      - A Node.js Framework
+  Body-Parser  - A tool to help use parse the data in a post request
+  Pg-Promise   - A database tool to help use connect to our PostgreSQL database
+***********************/
+var express = require('express'); //Ensure our express framework has been added
+var app = express();
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//Create Database Connection
+var pgp = require('pg-promise')();
+
+
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'btst_db',
+	user: 'postgres',
+	password: 'pwd'
+};
+
+var db = pgp(dbConfig);
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
+
