aeb7f07360acb6ce3dbca8063371f0f26cc97c56
Brian Peterson <bepetersn@gmail.com>
2019-04-18 19:08:01 -0600

    Add forgotten req here too

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
 const bcrypt = require('bcrypt');
 const busboy = require('connect-busboy');
-const uniqueFilename = require('unique-filename')
 
 // load other files 
 const utils = require('./utils.js');
--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const filenamify = require('filenamify');
-
+const uniqueFilename = require('unique-filename');
 
 module.exports = {
 
